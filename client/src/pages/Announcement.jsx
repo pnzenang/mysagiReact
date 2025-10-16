394cfc9525288e2b0364a37ca7fa2d1f7ec71789
pnzenang <pnzenang@gmail.com>
2025-10-16 08:50:54 -0400

    10.16.commit

--- client/src/pages/Announcement.jsx
+++ client/src/pages/Announcement.jsx
@@ -1,43 +1,53 @@
-import { toast } from 'react-toastify';
-import { AnnouncementSearchContainer } from '../components';
-import customFetch from '../utils/customFetch';
-import { useLoaderData } from 'react-router-dom';
-import { createContext, useContext } from 'react';
-import AnnouncementMembersContainer from '../components/AnnouncementMembersContainer';
+import { toast } from 'react-toastify'
+import { AnnouncementSearchContainer } from '../components'
+import customFetch from '../utils/customFetch'
+import { useLoaderData } from 'react-router-dom'
+import { createContext, useContext } from 'react'
+import AnnouncementMembersContainer from '../components/AnnouncementMembersContainer'
 
 export const loader = async ({ request }) => {
-  console.log(request.url);
+  console.log(request.url)
   const params = Object.fromEntries([
     ...new URL(request.url).searchParams.entries(),
-  ]);
+  ])
 
   try {
     const { data } = await customFetch.get('/members', {
       params,
-    });
-    return { data, searchValues: { ...params } };
+    })
+    return { data, searchValues: { ...params } }
   } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return error;
+    toast.error(error?.response?.data?.msg)
+    return error
   }
-};
-const AnnouncementContext = createContext();
+}
+const AnnouncementContext = createContext()
 
 const Announcement = () => {
-  const { data, searchValues } = useLoaderData();
+  const { data, searchValues } = useLoaderData()
 
   return (
-    <AnnouncementContext.Provider value={{ data, searchValues }}>
-      <h2 className='text-primary  md:text-xl'>
-        Delegate, check the name again to make sure you are announcing the right
-        death and bravely click on the button "announce dead" in the deceased
-        member&apos;s card to announce the death. may his or her soul rest in
-        peace.
-      </h2>
-      <AnnouncementSearchContainer />
-      <AnnouncementMembersContainer />
-    </AnnouncementContext.Provider>
-  );
-};
-export const useAnnouncementContext = () => useContext(AnnouncementContext);
-export default Announcement;
+    // <AnnouncementContext.Provider value={{ data, searchValues }}>
+    //   <h2 className='text-primary  md:text-xl'>
+    //     Delegate, check the name again to make sure you are announcing the right
+    //     death and bravely click on the button "announce dead" in the deceased
+    //     member&apos;s card to announce the death. may his or her soul rest in
+    //     peace.
+    //   </h2>
+    //   <AnnouncementSearchContainer />
+    //   <AnnouncementMembersContainer />
+    // </AnnouncementContext.Provider>
+    <h1 className='text-4xl py-36 '>
+      Please announce your death by sending an email to one of the following
+      SAGI emails:{' '}
+      <span>
+        info@sagiusa.org, info@sagi-transition.com or info@mysagi.org.
+      </span>
+      <br />
+      <br />
+      And be reminded that no documentation is needed while to announce a death.
+    </h1>
+  )
+}
+export const useAnnouncementContext = () => useContext(AnnouncementContext)
+export default Announcement
