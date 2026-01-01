b01799b846476a2ad847bb8dd5eff0a0a2c2832b
pnzenang <49215536+pnzenang@users.noreply.github.com>
2025-12-31 23:44:19 -0500

    Fix URL in AccordionItem for SAGI NIGERIA

--- client/src/pages/Fatherland.jsx
+++ client/src/pages/Fatherland.jsx
@@ -37,6 +37,7 @@ const Fatherland = () => {
             />
             <AccordionItem
               header='SAGI NIGERIA'
+                //text='Page Under construction'
               text='Click on the link below to register your loved ones living back in Nigeria with saginig.org in order to get together with other brothers and sisters  to make it easier if heaven for bits, one of them falls.'
               url='https//www.saginigeria.org'
             />
